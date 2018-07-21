b44e0840c07c678da17c4683ce778287d275173b
kenny-tech <kenny4real2001@gmail.com>
2018-07-21 10:40:12 +0100

    Set up express server

--- index.js
+++ index.js
@@ -9,4 +9,5 @@ app.get('/', function(req, res){
     res.send('hello world');
 });
 
-app.listen(3001, () => console.log('App listening on port 3001!'));
+app.listen(3001);
+console.log('App listening on port 3001!');
